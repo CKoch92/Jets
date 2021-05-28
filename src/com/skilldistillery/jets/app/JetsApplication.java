22723b807bb2ca4caa5e81e5155b3bfe2b5404f4
Caleb Koch <caleb.koch@gmail.com>
2021-05-28 15:23:25 -0600

    init commit

---
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.jets.app;
+
+import com.skilldistillery.jets.entity.AirField;
+
+public class JetsApplication {
+private AirField airfield;
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/skilldistillery/jets/entity/Jet.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.jets.entity;
+
+public abstract class Jet {
+private String model;
+private double speed;
+private int range;
+private long price;
+
+}
